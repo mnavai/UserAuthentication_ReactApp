a5b3df671876cd4376faf812b4ff97d5b406249e
Maryam Navaei <maryam.navaei@gmail.com>
2023-04-24 15:12:11 -0400

    index revised

--- src/index.js
+++ src/index.js
@@ -1,13 +1,37 @@
 import React from 'react';
+import Login from './components/Login';
+import Signup from './components/Signup';
+import Dashboard from './components/Dashboard';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+  },{
+    path: "/login",
+    element: <Login></Login>,
+  },{
+    path: "/dashboard",
+    element: <Dashboard></Dashboard>
+  },{
+    path: "/signup",
+    element: <Signup></Signup>,
+  }
+]);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
+    {/* <App /> */}
   </React.StrictMode>
 );
 
