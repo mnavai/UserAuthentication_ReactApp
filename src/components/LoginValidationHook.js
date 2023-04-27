0945ae84c5417608b1be4d574414e72e77151f81
Dheeraj Singh <dheeraj@careerera.com>
2023-04-27 19:48:21 +0530

    added the validation with api call

--- src/components/LoginValidationHook.js
+++ src/components/LoginValidationHook.js
@@ -4,10 +4,32 @@ import { useNavigate } from "react-router-dom";
 import { login } from "../utils/apiUtils";
 import {useForm} from "react-hook-form"
 const LoginHook = () => {
-  const { register, handleSubmit, watch, formState } = useForm();  
+  const { register, handleSubmit, watch, formState:{errors} } = useForm();  
   const [loading, setLoading] = useState(false); // Add a loading state
   const navigate = useNavigate();
-  const onSubmit = data=> console.log("FROM DATA ",data)
+  const onSubmit=(data)=>{ 
+
+    setLoading(true); // Set loading to true when the form is submitted
+    try {
+      const response =  login({
+        email: data.email,
+        password: data.password,
+      })
+      response.then(function (response) {
+        console.log(response.data);
+        localStorage.setItem("accessToken", response.data.response);
+        navigate("/dashboard");
+      }).catch(function (error) {
+        console.error(error);
+      });
+    } catch (error) {
+      console.error(error);
+      alert("Invalid username or password");
+    }
+    setLoading(false); // Set loading to false after the response is received
+
+
+  }
   return (
     <div className="container d-flex align-items-center justify-content-center">
       
@@ -40,9 +62,9 @@ const LoginHook = () => {
         </div>
         
       
-        {formState.errors.email && <span> This field is required </span>}
-      {formState.errors.password && <span> This field is required </span>}
-      {formState.errors.email?.message && <span>{ formState.errors.email?.message }</span>}
+        {errors.email && <span> This field is required </span>}
+      {errors.password && <span> This field is required </span>}
+      {errors.email?.message && <span>{errors.email?.message }</span>}
         <button type="submit" className="btn btn-pink" disabled={loading}> {/* Disable the button while loading */}
           {loading && <span className="spinner-border spinner-border-sm me-2" role="status" aria-hidden="true"></span>} {/* Render the spinner if loading */}
           Login
